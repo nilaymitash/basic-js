b503e5b5a95f5c2e22c062578c39c02674952f8f
Nilay Mitash <nilay_mitash@hotmail.com>
2021-06-04 12:40:11 -0400

    added label for tip percent

--- tip-calculator/tip-calculator.js
+++ tip-calculator/tip-calculator.js
@@ -1,9 +1,12 @@
 function calculateTip(tipPercent) {
-    var amt_before_tip = Number(document.getElementById("amt_before_tip").value);
+    document.getElementById("tip_%_lbl").innerHTML = "(" + tipPercent + "%)&nbsp;";
+
+    
+    var amt_before_tip = Math.round((Number(document.getElementById("amt_before_tip").value) + Number.EPSILON) * 100) / 100;
     
     var tipValue = (tipPercent/100)*amt_before_tip;
-    document.getElementById("tip_amt").value = tipValue;
+    document.getElementById("tip_amt").value = Math.round((tipValue + Number.EPSILON) * 100) / 100;
 
     var total = amt_before_tip + tipValue;
-    document.getElementById("total").value = total;
+    document.getElementById("total").value = Math.round((total + Number.EPSILON) * 100) / 100;
 }
