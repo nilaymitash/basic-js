822f938199743fec62f3c870168d5cc3ef53295f
Nilay Mitash <nilay_mitash@hotmail.com>
2021-06-07 18:56:32 -0400

    validation for expense tracker

--- expense-tracker/expense-tracker.js
+++ expense-tracker/expense-tracker.js
@@ -6,14 +6,52 @@ function addExpense() {
     var expenseDate = document.getElementById("expense-date").value;
     var expenseAmount = document.getElementById("expense-amount").value;
 
-    document.getElementById("expense-tbody").innerHTML += createExpenseRow(expenseName, expenseDate, expenseAmount);
+    if(validateExpense(expenseName, expenseDate, expenseAmount)) {
+        document.getElementById("expense-tbody").innerHTML += createExpenseRow(expenseName, expenseDate, expenseAmount);
+        clearFields();
+    }
+}
+
+function validateExpense(expenseName, expenseDate, expenseAmount) {
+    var isNameValid = validateName(expenseName);
+    var isDateValid = validateDate(expenseDate);
+    var isAmountValid = validateAmount(expenseAmount);
+    return isNameValid && isDateValid && isAmountValid;
+}
+
+function validateName(expenseName) {
+    if(expenseName.length < 1) {
+        document.getElementById("expense-name").style = "border-color: red; border-width: 1px;";
+        return false;
+    }
+    document.getElementById("expense-name").style = null;
+    return true;
+}
+
+function validateDate(expenseDate) {
+    if(expenseDate.length < 1) {
+        document.getElementById("expense-date").style = "border-color: red; border-width: 1px;";
+        return false;
+    }
+    document.getElementById("expense-date").style = null;
+    return true;
+}
+
+function validateAmount(expenseAmount) {
+    var amount = Number(expenseAmount);
+    if(isNaN(amount) || amount <= 0) {
+        document.getElementById("expense-amount").style = "border-color: red; border-width: 1px;";
+        return false;
+    }
+    document.getElementById("expense-amount").style = null;
+    return true;
 }
 
 function createExpenseRow(expenseName, expenseDate, expenseAmount) {
     var rowItem = "<tr id='tr_" +expenseCounter + "'>"
     + "<td>" + expenseName + "</td>"
     + "<td>" + expenseDate + "</td>"
-    + "<td> $" + expenseAmount + "</td>"
+    + "<td> $ " + expenseAmount + "</td>"
     + "<td><button type='button' class='btn btn-danger' onclick=\"removeThisItem('tr_" +expenseCounter+ "')\">Remove Expense</button></td>"
     +"</tr>";
 
@@ -23,4 +61,10 @@ function createExpenseRow(expenseName, expenseDate, expenseAmount) {
 function removeThisItem(id) {
     var element = document.getElementById(id);
     element.parentNode.removeChild(element);
+}
+
+function clearFields() {
+    document.getElementById("expense-name").value = '';
+    document.getElementById("expense-date").value = '';
+    document.getElementById("expense-amount").value = '';
 }
